dcd3d58e87ff868a069cc52d0394cda955528833
Jiri Popek <jiri.popek@gmail.com>
2019-06-26 23:39:04 +0200

    initial commit

---
+++ tof_vl53l1x.h
@@ -0,0 +1,40 @@
+#include "esphome.h"
+
+#include <Wire.h>
+#include <VL53L1X.h>
+
+VL53L1X tof_sensor;
+
+class MyCustomSensor : public PollingComponent, public Sensor {
+ public:
+  // constructor
+  MyCustomSensor() : PollingComponent(15000) {} //60*60000) {}  // polling every hour
+
+  void setup() override {
+    // This will be called by App.setup()
+    Wire.begin();
+    Wire.setClock(400000); // use 400 kHz I2C
+
+    tof_sensor.setTimeout(500);
+    tof_sensor.setAddress(0x29);
+    if (!tof_sensor.init()) {
+      ESP_LOGE("VL53L1X custom sensor", "Failed to detect and initialize sensor!");
+      while (1);
+    }
+
+    tof_sensor.setDistanceMode(VL53L1X::Long);
+    tof_sensor.setMeasurementTimingBudget(50000);
+
+    tof_sensor.startContinuous(50);
+  }
+
+  void update() override {
+    uint16_t mm = tof_sensor.read();
+    
+    if (!tof_sensor.timeoutOccurred()) {
+      publish_state(mm);
+    } else {
+      ESP_LOGE("VL53L1X custom sensor", "Timeout during readRangeSingleMillimeters().");
+    }
+  }
+};
